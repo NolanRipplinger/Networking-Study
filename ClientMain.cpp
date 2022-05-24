e17937307bc9df9ad2f160617b8d4294e64caf54
Nolan Ripplinger <nolan.ripplinger@gmail.com>
2022-05-24 13:21:49 -0500

    Stack around buf and service get corrupted

--- ClientMain.cpp
+++ ClientMain.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include "NolanNetworking.h"
 
+const int MAX_BUFFER_LENGTH = 4096; //Tutorial default was 4096
+const int PORT = 1738; 
 
 
 int main() {
@@ -57,7 +59,7 @@ int main() {
 	*/
 	sockaddr_in hint;
 	hint.sin_family = AF_INET;
-	hint.sin_port = htons(1738);
+	hint.sin_port = htons(PORT);
 	hint.sin_addr.S_un.S_addr = INADDR_ANY; //Could also use inet_pton and bind to local loopback address
 
 	bind(listening, (sockaddr*)&hint, sizeof(hint));
@@ -76,25 +78,73 @@ int main() {
 	sockaddr_in client;
 	int clientSize = sizeof(client);
 
-	SOCKET clientsocket = accept(listening, (sockaddr*)&client, &clientSize);
+	SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+	if (clientSocket == INVALID_SOCKET) 
+	{
+		std::cerr << "Can't accept socket, exiting" << std::endl;
+		return 3;
+	}
+
+	char host[NI_MAXHOST];		//Client's remote name
+	char service[NI_MAXSERV];	//Service (port) being connected on
+
+	ZeroMemory(host, NI_MAXHOST);	//target, size
+	ZeroMemory(service, NI_MAXHOST);	//target, size
+
+	if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0) 
+	{
+		std::cout << host << ": connected on port " << service << std::endl;
+	}
+	else //get ip of host if can't find name
+	{
+		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+		std::cout << host << ": connected on port " << service << std::endl;
+	}
 
 	/* Close listening Socket
 	*
 	*/ 
+	closesocket(listening);
 
 
 	/* While: acceptand echo message back to cleitn
 	*
 	*/
+	char buf[MAX_BUFFER_LENGTH];
+
+	while (true)//Run server forever 
+	{
+		ZeroMemory(buf, MAX_BUFFER_LENGTH);
+
+		//Wait for client to send data
+		int bytesReceived = recv(clientSocket, buf, MAX_BUFFER_LENGTH, 0);
+		if (bytesReceived == SOCKET_ERROR) 
+		{
+			std::cerr << "Error in recv(), exiting" << std::endl;
+			break;
+		}
+
+		if (bytesReceived == 0) 
+		{
+			std::cout << "client disconnected" << std::endl;
+			break;
+		}
+
+		//Echo back to client
+		send(clientSocket, buf, bytesReceived + 1, 0);//add for terminating string
+
+	}
 
 	
 	/* Close socket
 	*
 	*/ 
+	closesocket(clientSocket);
 
-	/* Shutdown winsock
+	/* Clean up winsock
 	*
-	*/ 
+	*/
+	WSACleanup();
 
 	return 0;
 }
